90fcfdf1e016b5216228b207ad7946526d3c6b5a
Amelia <StarshipAmelia@gmail.com>
2016-12-15 17:55:00 -0500

    Inital commit, barebones

---
+++ adb_statusline.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""adb_statusline.py is a reasonably simple script that will print a number of
+statistics about the connected android phone, via adb. These are customizable.
+Further information will be added later..."""
+
+# Imports go here
+
+# Authorship information
+__author__ = "Ameliai"
+__copyright__ = "2016, Amelia"
+__credits__ = ["Amelia"]
+
+__license__ = "GPLv3"
+__version__ = "0.0.1"
+__maintainer__ = "Amelia"
+__email__ = "StarshipAmelia@gmail.com"
+__status__ = "Prototype"
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
